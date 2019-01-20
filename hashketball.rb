6702f6f31dde0d6776701278362538d30017a8fe
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-20 19:05:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,7 +156,7 @@ end
 def player_numbers(team_name)
   game_hash.each do |location, info|
       if info[:team_name] == team_name
-        return info[:players].map {
+        return info[:players].map {|player| player[:number]}
       end
     end
   end
