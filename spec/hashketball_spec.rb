54b9bf77a7cc271159b7d52663657197d7c0745b
Amy Cohen <amy.cohen@flatironschool.com>
2019-04-13 16:41:54 -0700

    Done.

--- spec/hashketball_spec.rb
+++ spec/hashketball_spec.rb
@@ -250,21 +250,21 @@ end
 
 # describe 'bonus' do
 
-#   describe '#most_points_scored' do
+  describe '#most_points_scored' do
 
-#     it 'returns Ben Gordon' do
-#       expect(most_points_scored).to eq("Ben Gordon")
-#     end
+    it 'returns Ben Gordon' do
+      expect(most_points_scored).to eq("Ben Gordon")
+    end
 
-#   end
+  end
 
-#   describe '#winning_team' do
+  describe '#winning_team' do
 
-#     it 'returns the Brooklyn Nets' do
-#       expect(winning_team).to eq("Brooklyn Nets")
-#     end
+    it 'returns the Brooklyn Nets' do
+      expect(winning_team).to eq("Brooklyn Nets")
+    end
 
-#   end
+  end
 
 #   describe '#player_with_longest_name' do
 
